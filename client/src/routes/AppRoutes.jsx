a188fcacd7f4b4a1da4ecde0aa27dc90cecc1e54
Himmat913 <himantsidhu2005@gmail.com>
2026-07-17 20:14:17 +0530

    Integrate seller routes into AppRoutes

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -7,17 +7,28 @@ import LiveSession from "../pages/LiveSession";
 import Checkout from "../pages/Checkout";
 import OrderSuccess from "../pages/OrderSuccess";
 
+import SellerDashboard from "../pages/SellerDashboard";
+import LiveStudio from "../pages/LiveStudio";
+import Analytics from "../pages/Analytics";
+
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Landing */}
         <Route path="/" element={<Landing />} />
+
+        {/* Customer Routes */}
         <Route path="/customer/home" element={<CustomerHome />} />
         <Route path="/customer/discovery" element={<LiveDiscovery />} />
         <Route path="/customer/live" element={<LiveSession />} />
         <Route path="/customer/checkout" element={<Checkout />} />
-        <Route path="/seller/dashboard" element={<h1>Seller Dashboard</h1>} />
         <Route path="/customer/order-success" element={<OrderSuccess />} />
+
+        {/* Seller Routes */}
+        <Route path="/seller/dashboard" element={<SellerDashboard />} />
+        <Route path="/seller/live-studio" element={<LiveStudio />} />
+        <Route path="/seller/analytics" element={<Analytics />} />
       </Routes>
     </BrowserRouter>
   );
